b41525949d4a015d466aee55fa48cecb075e3722
drimnas <clovismichaud1287@gmail.com>
2021-02-10 17:24:31 +0100

    mouvement joueur

--- Assets/Script/PlayerMouvement.cs
+++ Assets/Script/PlayerMouvement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMouvement : MonoBehaviour
 {
+    private float horizontal;
+    public float speed;
+    public Vector2 movement;
+    private float vertical;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class PlayerMouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        movement.x = horizontal * speed;
+        movement.y = vertical * speed;
         
+        transform.Translate( movement * Time.deltaTime); 
     }
 }
