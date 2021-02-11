fd910672697696b87155cf2abd5c505e33776f46
Lelio <lelio.rollin@e-artsup.net>
2021-02-11 17:24:02 +0100

    Player Health

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool paused;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth;
+    public float actualHealth;
+
+    public Slider healthBar;
+    
+    
+    void Start()
+    {
+        actualHealth = maxHealth;
+
+        healthBar.highValue = maxHealth;
+        healthBar.value = actualHealth;
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        actualHealth -= damage;
+
+        healthBar.value = actualHealth;
+        
+        if (actualHealth <= 0)
+        {
+            Death();
+        }
+    }
+    
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+}
