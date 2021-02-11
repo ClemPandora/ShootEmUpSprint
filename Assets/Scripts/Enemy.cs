14300b8411c8faafcef743c542f01d5d85f6560d
Pandorage <clement.pontoizeau@hotmail.fr>
2021-02-11 17:20:13 +0100

    Ennemi spawner

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,25 @@ public class Enemy : MonoBehaviour
     public float verticalSpeed;
     public float amplitude;
     private Vector2 _startPos;
+    private float _delay;
+    private Vector2 _screenBounds;
+    private float _objectHeight;
     private void Start()
     {
         _startPos = transform.position;
+        _delay = Random.Range(0f, 10f);
+        _screenBounds =
+            Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        _objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
     }
 
     void Update()
     { 
-        transform.position =new Vector2(_startPos.x + Mathf.Sin(Time.time * horizontalSpeed) * amplitude, transform.position.y - verticalSpeed * Time.deltaTime);
+        transform.position = new Vector2(_startPos.x + Mathf.Sin(Time.time * horizontalSpeed + _delay) * amplitude, transform.position.y - verticalSpeed * Time.deltaTime);
+        if (transform.position.y < -_screenBounds.y - _objectHeight)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public float spawnRate = 0.5f;
+    private Vector2 _screenBounds;
+    public Enemy[] enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        InvokeRepeating(nameof(Spawn),0,spawnRate);
+    }
+    
+    void Spawn()
+    {
+        var enemy = enemies[Random.Range(0,enemies.Length)];
+        var objectWidth = enemy.transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        var objectHeigth = enemy.transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+        Instantiate(enemy, new Vector2(Random.Range(-_screenBounds.x + objectWidth, _screenBounds.x - objectWidth), _screenBounds.y + objectHeigth), transform.rotation);
+    }
+}
