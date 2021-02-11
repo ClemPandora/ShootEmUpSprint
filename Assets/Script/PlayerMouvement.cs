a4978cf9bbfe08670ef5481536456499758ab683
Pandorage <clement.pontoizeau@hotmail.fr>
2021-02-11 17:30:25 +0100

    Clamp player to screen

--- Assets/Script/PlayerMouvement.cs
+++ Assets/Script/PlayerMouvement.cs
@@ -10,10 +10,18 @@ public class PlayerMouvement : MonoBehaviour
     public float speedMax;
     public Vector2 movement;
     private float vertical;
+    private Vector2 _screenBounds;
+
+    private float _objectWidth;
+    private float _objectHeight;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         speed = speedInitial;
+        _objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        _objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -37,5 +45,13 @@ public class PlayerMouvement : MonoBehaviour
         {
             speed = Mathf.Lerp(speedMax, speedInitial, 3);
         }
+    }
+    
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, _screenBounds.x * -1 + _objectWidth, _screenBounds.x - _objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, _screenBounds.y * -1 + _objectHeight, _screenBounds.y - _objectHeight);
+        transform.position = viewPos;
     }
 }
