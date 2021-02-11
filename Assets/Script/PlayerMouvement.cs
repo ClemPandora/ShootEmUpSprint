9fafdb2936c71ec5c2d66f3a5e211760cce57387
drimnas <clovismichaud1287@gmail.com>
2021-02-11 17:17:16 +0100

    mouvement smooth

--- Assets/Script/PlayerMouvement.cs
+++ Assets/Script/PlayerMouvement.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerMouvement : MonoBehaviour
 {
     private float horizontal;
-    public float speed;
+    public float speedInitial;
+    private float speed;
+    public float speedMax;
     public Vector2 movement;
     private float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = speedInitial;
     }
 
     // Update is called once per frame
@@ -20,9 +22,20 @@ public class PlayerMouvement : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        movement.x = horizontal * speed;
-        movement.y = vertical * speed;
-        
-        transform.Translate( movement * Time.deltaTime); 
+        movement.x = horizontal;
+        movement.y = vertical;
+        movement = movement.normalized * speed;
+
+        transform.Translate( movement * Time.deltaTime);
+
+        if (Input.GetButton("Jump"))
+        {
+            speed = Mathf.Lerp(speedInitial, speedMax, 100);
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            speed = Mathf.Lerp(speedMax, speedInitial, 3);
+        }
     }
 }
