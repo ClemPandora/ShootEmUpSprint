2ee6029095c600bd3d264af9a34439e3666eefd3
Pandorage <clement.pontoizeau@hotmail.fr>
2021-04-01 17:35:53 +0200

    HealthUp on score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,7 +7,11 @@ public class Score : MonoBehaviour
 {
     public int score;
 
+    public int toLife = 1000;
+
     public Text txt;
+
+    public PlayerHealth health;
 
     // Update is called once per frame
     void Update()
@@ -18,5 +22,11 @@ public class Score : MonoBehaviour
     public void AddScore(int toAdd)
     {
         score += toAdd;
+        toLife -= toAdd;
+        if (toLife <= 0)
+        {
+            toLife += 1000;
+            health.HealthUp();
+        }
     }
 }
