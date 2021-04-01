5b3b2298c79f3f9ca5c950f6e55a2aeda390136e
Pandorage <clement.pontoizeau@hotmail.fr>
2021-04-01 16:41:53 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Lelio");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
